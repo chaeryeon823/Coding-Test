1bbffad75116cca4e3e76f9de98ee1639bf7ff25
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-03-26 23:57:53 +0900

    [BOJ] 16953 A->B

---
+++ BOJ/16953.py
@@ -0,0 +1,26 @@
+# A->B
+import sys
+
+a, b = map(int, sys.stdin.readline().split())
+
+answer = 0
+
+while b > a:
+
+  temp = b
+
+  if b % 10 == 1:
+    b = b // 10
+    answer += 1
+  elif b % 2 == 0:
+    
+    b = b // 2
+    answer += 1
+
+  if b == temp:
+    answer = -1
+    break
+  
+
+print(answer)
+
