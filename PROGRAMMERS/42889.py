8de3f0aff31a734874814d5dbb33b5c8639d8ce2
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-04-16 00:24:34 +0900

    [PSG] 42889 / 실패율

---
+++ PROGRAMMERS/42889.py
@@ -0,0 +1,17 @@
+def solution(N, stages):
+    answer = []
+    arr = [0 for i in range(N + 1)]
+    dict = {}
+    
+    tmp = len(stages)
+    
+    for i in range(N):
+        cnt = stages.count(i + 1)
+        if cnt != 0:
+            dict[i + 1] = cnt / tmp
+            tmp = tmp - cnt
+        else:
+            dict[i + 1] = 0
+
+    answer = sorted(dict, key=lambda x:-dict[x])
+    return answer
