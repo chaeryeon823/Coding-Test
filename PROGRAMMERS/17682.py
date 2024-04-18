9e2aa6f39b1ab10e43fa63e457b2ce0670245a24
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-04-18 23:37:52 +0900

    [PSG] 17682 / 1차 다트게임 추가

--- PROGRAMMERS/17682.py
+++ PROGRAMMERS/17682.py
@@ -22,3 +22,21 @@ def solution(dartResult):
             score[index - 1] *= 2
 
     return sum(score)
+
+
+
+# import re
+
+
+# def solution(dartResult):
+#     bonus = {'S' : 1, 'D' : 2, 'T' : 3}
+#     option = {'' : 1, '*' : 2, '#' : -1}
+#     p = re.compile('(\d+)([SDT])([*#]?)')
+#     dart = p.findall(dartResult)
+#     for i in range(len(dart)):
+#         if dart[i][2] == '*' and i > 0:
+#             dart[i-1] *= 2
+#         dart[i] = int(dart[i][0]) ** bonus[dart[i][1]] * option[dart[i][2]]
+
+#     answer = sum(dart)
+#     return answer
