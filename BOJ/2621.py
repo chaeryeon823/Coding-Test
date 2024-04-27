251ef487b1d208595706f92f3e1e29b18b881da9
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-04-28 01:04:18 +0900

    [BOJ] 2621 / 카드게임

--- BOJ/2621.py
+++ BOJ/2621.py
@@ -2,18 +2,22 @@
 # S3
 
 import sys
+from collections import Counter 
 
 def all_different_num(num):
   return len(set(num)) == len(num)
 
 def continuous_num(num):
   for i in range(5):
-    if num.find(num[i] - 1) > -1 or num.find(num[i] + 1) > -1:
+    if (num[i] + 1) not in num or (num[i] - 1) not in num:
       return False
     return True
 
 def same_color(col):
   return len(set(col)) == 1
+
+def sort_dict(c):
+  return sorted(dict(c).items(), key=lambda x:x[1], reverse=True)
 
 color = []
 number = []
@@ -23,5 +27,43 @@ for i in range(5):
   color.append(tmp[0])
   number.append(int(tmp[1]))
 
-  if all_different_num(number):
-    if 
+if all_different_num(number):
+  if continuous_num(number):
+    if same_color(color):
+      # 1
+      print(max(number) + 900)
+    else:
+      # 5
+      print(max(number) + 500)
+  else:
+    if same_color(color):
+      # 4
+      print(max(number) + 600)
+    else:
+      # 9
+      print(max(number) + 100)
+else:
+  c = Counter(number)
+  cnt = dict(c).values()
+
+  if 4 in cnt:
+    # 2
+    print(c.most_common(n=1)[0][0] + 800)
+  else:
+    t = sort_dict(c)
+    if 3 in cnt:
+      if 2 in cnt:
+        # 3
+        print((t[0][0] * 10) + t[1][0] + 700)
+      else:
+        # 6
+        print((t[0][0] + 400))
+    else:
+      if len(cnt) == 3:
+        # 7
+        max_num = max(t[0][0], t[1][0])
+        min_num = min(t[0][0], t[1][0])
+        print((max_num * 10) + min_num + 300)
+      else:
+        # 8
+        print(c.most_common(n=1)[0][0] + 200)
