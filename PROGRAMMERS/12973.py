b87bfdf487e77189d4d85f722bc6d7c2164f4881
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-05-09 12:30:28 +0900

    [PGS] 12973 / 짝지어 제거하기 / lv 2

---
+++ PROGRAMMERS/12973.py
@@ -0,0 +1,19 @@
+# 짝지어 제거하기
+# lv 2
+def solution(s):
+    stack = []
+    stack.append(s[0])
+
+    for i in range(1, len(s)):
+        if stack:
+            if stack[-1] == s[i]:
+                stack.pop()
+            else:
+                stack.append(s[i])
+        else:
+            stack.append(s[i])           
+
+    if stack:
+        return 0
+    else:
+        return 1
