f1770e98ce1d1d289494e27e612fecc516d927ae
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-05-22 22:17:48 +0900

    [PGS] 76502 / 괄호 회전하기 / lv 2

---
+++ PROGRAMMERS/76502.py
@@ -0,0 +1,33 @@
+# 괄호 회전하기
+# lv 2
+# stack 스택
+
+def check_bracket(st):
+    cl = [']', ')', '}']
+    op = ['[', '(', '{']
+    stack = []
+    
+    for c in st:
+        if c == op[0] or c == op[1] or c == op[2]:
+            stack.append(c)
+        else:
+            if len(stack) == 0:
+                return False
+            else:
+                if cl.index(c) == op.index(stack[-1]):
+                    stack.pop()
+                else:
+                    return False
+    if len(stack) == 0:
+        return True
+        
+    return False
+    
+
+def solution(s):
+    answer = 0
+    l = len(s)
+    s = s * 2
+    for i in range(l):
+        answer += check_bracket(s[i:i+l])
+    return answer
