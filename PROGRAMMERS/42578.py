f572a63d907657701db734a757fa80060665ee75
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-05-24 10:26:39 +0900

    [PGS] 42578 / 의상 / lv 2

---
+++ PROGRAMMERS/42578.py
@@ -0,0 +1,18 @@
+# 의상
+# lv 2
+# 수학, 딕셔너리, dictionary
+
+def solution(clothes):
+    answer = 1
+    dictionary = {}
+    
+    for _, ctype in clothes:
+        if ctype not in dictionary.keys():
+            dictionary[ctype] = 1
+        else:
+            dictionary[ctype] += 1
+    
+    for value in dictionary.values():
+        answer *= (value + 1)
+    
+    return answer - 1
