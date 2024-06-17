e21977acb8c2ca08dff38fc60197b9df73c92a6e
Kang Chaeryeon <87600308+chaeryeon823@users.noreply.github.com>
2024-06-17 23:29:44 +0900

    [PGS] 155652 / 둘만의 암호 / lv 1

---
+++ PROGRAMMERS/155652.py
@@ -0,0 +1,23 @@
+# 둘만의 암호
+# lv1
+# 97 ~ 122
+def solution(s, skip, index):
+    answer = ''
+    cnt = 0
+    for c in s:
+        tmp = ord(c)
+        while cnt < index:
+            tmp = tmp + 1
+            if tmp > 122:
+                tmp = (tmp - 122) + 96
+            if chr(tmp) not in list(skip):
+                cnt += 1
+
+        cnt = 0
+        print(chr(tmp))
+        answer += chr(tmp)
+                
+            
+    return answer
+
+print(solution("aukks",	"wbqd",	5))
